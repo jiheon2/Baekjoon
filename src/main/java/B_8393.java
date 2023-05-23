abe1c1b16025e331f12047bd2a66afb808b797f2
KimJiHeon <000224kjh@gmail.com>
2023-05-23 22:40:06 +0900

    백준

--- src/main/java/B_8393.java
+++ src/main/java/B_8393.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class B_8393 {
+import java.util.Scanner;
+
+public class B_8393 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n;
+        int hap = 0;
+
+        n = s.nextInt();
+
+        for(int i=1; i<=n; i++) {
+            hap += i;
+        }
+        System.out.print(hap);
+    }
 }
